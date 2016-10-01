bcbc5730f9ec690d3fb1d21c2a6feec641d6b512
isabelcosta <isabelcmdcosta@gmail.com>
2016-10-01 14:20:11 +0000

    new entities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001005859) do
+ActiveRecord::Schema.define(version: 20161001120238) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string  "title"
+    t.string  "category"
+    t.string  "skills"
+    t.integer "organization_id"
+    t.text    "description"
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string  "title"
+    t.text    "description"
+    t.integer "user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string  "title"
