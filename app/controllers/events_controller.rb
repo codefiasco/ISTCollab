776ecde63d7bdadf1fc43475ccb8cc573707dc6f
isabelcosta <isabelcmdcosta@gmail.com>
2016-10-01 22:15:51 +0000

    events in progress

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,44 @@
+class EventsController < ApplicationController
+  before_action :current_user
+  
+  def new
+    @event = Event.new()
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.organization = Organization.find(session[:organization_id])
+    if @event.save
+      redirect_to organization_path()
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @events = Event.all()
+  end
+
+  def edit
+    set_event
+  end
+
+  def show
+    set_event
+  end
+
+  def my_projects
+    current_user
+    @myProjects = @current_user.projects
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :description, :skills, :category)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161001160509) do
+ActiveRecord::Schema.define(version: 20161001163620) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
