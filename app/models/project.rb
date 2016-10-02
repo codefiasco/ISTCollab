aff7db475a32216eaceff2d4095949da6a8c92b0
Francisco Machado <franm912@gmail.com>
2016-10-02 15:28:18 +0100

    Added default images to projects and events

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,7 +11,7 @@ class Project < ActiveRecord::Base
   validates :category, presence: true
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100<" },
-                             default_url: ":style/default-avatar.png"
+                             default_url: ":style/project.jpg"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
