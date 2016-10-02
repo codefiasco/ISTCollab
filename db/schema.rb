462db94752227e2462ad530a8f775c4e42341a5f
Francisco Machado <franm912@gmail.com>
2016-10-02 11:57:09 +0100

    Added social links to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002043345) do
+ActiveRecord::Schema.define(version: 20161002103306) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -31,6 +40,16 @@ ActiveRecord::Schema.define(version: 20161002043345) do
   create_table "interests", force: :cascade do |t|
     t.integer "user_id"
     t.integer "project_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -68,6 +87,8 @@ ActiveRecord::Schema.define(version: 20161002043345) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "github_link"
+    t.string   "linkedin_link"
   end
 
 end
